d885bc9710e0a0b48161ba993175977b93cb3a0a
John Martens <John.Martens@gmail.com>
2015-09-11 10:24:52 -0400

    Finished Momtum assignment.

--- src/Momentum.java
+++ src/Momentum.java
@@ -1,9 +1,25 @@
+import hsa.*;
 
 public class Momentum {
 
-  
     public static void main(String[] args) {
+      Console c = new Console();
+      double mass,velocity,momentum;
+      c.print("Enter object's mass: ");
+      mass = c.readDouble();
+      c.print("Enter object's velocity: ");
+      velocity=c.readDouble();
+      //calculte answer
+      momentum = mass * velocity;
+      //show result
+      c.print("The momentum is " + momentum );
       
     }
     
 }
+
+
+
+
+
+
